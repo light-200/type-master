40acc26ede8bd154348252c9ad8b35a415670ecc
light-200 <aryanshaily123@gmail.com>
2021-11-02 23:14:04 +0530

    added comments for better understanding

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const textContainer = document.querySelector(".text");
 const words = document.querySelector(".words");
 const speed = document.querySelector(".speed");
-var text = textContainer.innerText;
 var totalWords = 0;
+var text;
+
+var getText = () => {
+    text = textContainer.innerText;
+    //call the api here 
+}
 
 const speedCalc = (totalWords, seconds) =>{
     var minutes = seconds.toPrecision(2) / 60;
     // console.log( totalWords , " " , minutes.toPrecision(2))
     let tempSpeed = totalWords / minutes;
     speed.innerText = tempSpeed.toPrecision(3);
+
+    // add a function to change the text here 
 }
 
+//wraps all words in a span tag
 const spanWrap = () => {
     let newArr = [];
     for(let i=0; i<text.length; i++){
@@ -20,6 +28,7 @@ const spanWrap = () => {
     textContainer.innerHTML = newText;
 }
 
+//sets total word value
 const setWords = (typedWords = 0) => {
     let whitespace = / /g,result,indices = [];
     while(result = whitespace.exec(text)){
@@ -29,7 +38,7 @@ const setWords = (typedWords = 0) => {
 
     words.innerText = `${totalWords}/${typedWords}`;
 }
-
+// listents to the keyboard
 const start = () => {
     let firstCall = true;
     let tempText = text;
@@ -65,6 +74,7 @@ const start = () => {
 
 }
 
+getText();
 setWords();
 spanWrap();
 start();
