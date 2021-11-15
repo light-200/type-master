a39794f3e17048ac37ccbfdf8e6ddcc082b973e4
light-200 <aryanshaily123@gmail.com>
2021-11-15 21:44:26 +0530

    undo rename

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: './src/script.js',
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     watch: true
