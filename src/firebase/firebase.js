9a038882bedbcbed5d6339d891bbce9da252a765
light-200 <44830619+light-200@users.noreply.github.com>
2022-01-24 13:42:42 +0530

    fix/added env variables 🧑‍⚕️

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEADUREMENT_ID,
+});
+
+const analytics = getAnalytics(app);
+
+export { app };
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 module.exports = {
-    mode: 'production',
-    entry: './src/script.js',
-    output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'bundle.js'
-    },
-    watch: true,
-}
+  mode: "production",
+  entry: "./src/script.js",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: "bundle.js",
+  },
+  plugins: [new Dotenv()],
+  watch: true,
+};
