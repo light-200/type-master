51bb934e50bb4fa3af6273e2d88f8489c7dc8d36
light-200 <44830619+light-200@users.noreply.github.com>
2022-01-27 22:35:39 +0530

    feat/basic functionality added 🥳

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express, { json } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("createRoom", (user, room) => createRoom(user, room, socket));
+  console.log("a user connected");
+});
+
+httpServer.listen(process.env.PORT || 3000, () => {
+  console.log("http://localhost:3000");
+});
+
+function createRoom(user, room, socket) {
+  let roomId = room.slice(0, 6);
+  socket.join(roomId);
+  user = JSON.parse(user);
+  socket.emit("roomId", roomId);
+}
