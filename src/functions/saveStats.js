2cd55723b51749e7cd83367a656bc2033d5c9a66
light-200 <44830619+light-200@users.noreply.github.com>
2022-02-01 19:18:02 +0530

    fix/dom to image package changed 🐛

--- src/functions/saveStats.js
+++ src/functions/saveStats.js
@@ -1,10 +1,13 @@
 import { saveAs } from "file-saver";
-import domtoimage from "dom-to-image";
+import domtoimage from "dom-to-image-more";
 import { stats } from "../ui/uiElements";
+import { getUserData } from "../storage/localstorage";
 
 function saveStats() {
-  domtoimage.toBlob(stats).then((blob) => {
-    window.saveAs(blob, "stats.png");
+  domtoimage.toBlob(stats).then(async (blob) => {
+    let user = await getUserData();
+    let name = user.userName + user.topSpeed;
+    window.saveAs(blob, name);
   });
 }
 
